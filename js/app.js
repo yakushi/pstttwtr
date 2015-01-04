430742ac6d0decae0ac83be7c11fe5fb79d49b2b
Takeshi Kurosawa <taken.spc@gmail.com>
2015-01-04 16:00:45 +0900

    Update app

--- js/app.js
+++ js/app.js
@@ -2,37 +2,23 @@ var PSTTTWTR = (function() {
   "use strict";
   return {
     TWITTER_URL: "https://twitter.com/intent/tweet",
-    textInput: null,
-    textButton: null,
 
     init: function() {
-      var it = this;
-      navigator.mozSetMessageHandler("activity", function(arg) { it.handleActivity(arg); });
-      window.addEventListener("load", this, false);
+      navigator.mozSetMessageHandler("activity", (arg) => {
+        this.handleActivity(arg);
+      });
     },
 
     handleActivity: function(activityRequest) {
-      this.disableUI(true);
       var option = activityRequest.source;
       if (option.name === "share") {
         var data = option.data;
         if (data && data.type === "url" && data.url) {
           this.tweet("", data.url);
+          activityRequest.postResult(null);
         } else {
           console.warn("unexpected activityRequest", data);
-        }
-      }
-      this.disableUI(false);
-    },
-
-    handleEvent: function(e) {
-      if (e.type === "load") {
-        this.disableUI(false);
-        this.textButton.addEventListener("click", this, false);
-      } else if (e.type === "click") {
-        var text = "" + this.textInput.value;
-        if (text !== "") {
-          this.tweet(text, "");
+          activityRequest.postError(null);
         }
       }
     },
@@ -49,17 +35,6 @@ var PSTTTWTR = (function() {
         params.append("url", url);
       }
       window.open(tweet.href, "_blank");
-    },
-
-    disableUI: function(disabled) {
-      if (this.textButton === null) {
-        this.textButton = document.getElementById("textButton");
-      }
-      this.textButton.disabled = disabled;
-      if (this.textInput === null) {
-        this.textInput = document.getElementById("textInput");
-      }
-      this.textInput.disabled = disabled;
     }
   };
 })();
